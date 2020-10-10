2df54eb195903195d04478ca8d38902e21aad09c
Bonjk <bonjk87@gmail.com>
2020-10-10 21:49:10 +0800

    add upload

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-from flask import Flask , render_template , request , url_for , redirect , flash
+import datetime
+import os
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_login import (LoginManager, UserMixin, current_user, login_required,
+                         login_user, logout_user)
 from flask_script import Manager
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from pypinyin import lazy_pinyin
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:/Users/遠邦/Desktop/軟工/flask_test/upload'
+ALLOWED_EXTENSIONS = set(["docx" , "pdf" , "csv" , "jpg"])
+
 app = Flask(__name__ , static_url_path = '/templates')
 manager = Manager(app)
 app.config["JSON_AS_ASCII"] = False
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
 app.debug = True
 app.secret_key = app.config.get('flask' , 'b9696332895257bb088ba05ca8fc6bd9')
 
 @app.route('/' , methods = ['POST' , 'GET'])
 def index():
-	return render_template("index.html")
-
-@app.route('/order' , methods = ['POST'])
-def order():
-	username = request.form['usr']
-	password = request.form['psd']
-	print('帳號:')
-	print(username)
-	print('密碼:')
-	print(password)
-	return render_template("login.html" , usr = username , psd = password)
+	if request.method == "POST":
+		file = request.files["upfile"]
+		if file and allowed_file(file.filename):
+			filename = secure_filename(time_stamp() + "".join(lazy_pinyin(file.filename)))
+			file.save(os.path.join(app.config["UPLOAD_FOLDER"] , filename))
+		else:
+			flash("不對喔")
+	return render_template("index.html" , Hi = "JiJi")
 
 @app.route('/login' , methods = ["GET" , "POST"])
 def login():
@@ -76,6 +85,15 @@ def request_loader(request):
 	user.id = user_now
 	user.is_authenticated = request.form["psd"] == users[user_now]["password"]
 	return user
+
+def allowed_file(filename):
+	'''check the extension of file, return True if legal, otherwise False'''
+	return "." in filename and filename.rsplit("." , 1)[1] in ALLOWED_EXTENSIONS
+
+def time_stamp():
+	d = datetime.datetime.now()
+	dnow = f"%04d-%02d-%02d-%02d-%02d-%02d_" % (d.year , d.month , d.day , d.hour , d.minute , d.second)
+	return dnow
 
 users = {"Bang" : {"password" : "40747016S"}}
 
